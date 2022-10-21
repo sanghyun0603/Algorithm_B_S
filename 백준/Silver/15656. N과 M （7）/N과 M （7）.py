c54b76beeedc583ab278d8a9a7fdde61cd6fcfbc
sanghyun0603 <110641935+sanghyun0603@users.noreply.github.com>
2022-10-21 13:53:54 +0900

    [Silver III] Title: N과 M (7), Time: 928 ms, Memory: 136136 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.py
@@ -0,0 +1,15 @@
+def seven(n):
+    if len(jo)==M:
+        print(*jo)
+        return
+    for i in range(N):
+        jo.append(num_li[i])
+        seven(i)
+        jo.pop()
+
+
+N , M = map(int,input().split())
+num_li = list(map(int,input().split()))
+num_li.sort()
+jo = []
+seven(0)
