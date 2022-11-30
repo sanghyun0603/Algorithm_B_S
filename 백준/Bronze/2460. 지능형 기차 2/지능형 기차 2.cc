73c805842c49dca447b8022eb94053000ccd492f
sanghyun0603 <110641935+sanghyun0603@users.noreply.github.com>
2022-11-30 23:24:30 +0900

    [Bronze III] Title: 지능형 기차 2, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2460. 지능형 기차 2/지능형 기차 2.cc
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int a,b;
+    int sum=0;
+    int max=0;
+    for(int i=0; i<10; i++){
+    scanf("%d %d",&a,&b);
+ 
+    sum+= -a+b;
+ 
+    if(sum>max)
+    max=sum;
+    }
+    printf("%d",max);
+}
