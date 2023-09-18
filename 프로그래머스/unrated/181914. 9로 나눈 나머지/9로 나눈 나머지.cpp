7f6a0d2a8d15e380beb03ce339904125c65b7c92
sanghyun0603 <110641935+sanghyun0603@users.noreply.github.com>
2023-09-18 11:37:05 +0900

    [unrated] Title: 9로 나눈 나머지, Time: 0.01 ms, Memory: 3.59 MB -BaekjoonHub

--- 프로그래머스/unrated/181914. 9로 나눈 나머지/9로 나눈 나머지.cpp
+++ 프로그래머스/unrated/181914. 9로 나눈 나머지/9로 나눈 나머지.cpp
@@ -5,10 +5,5 @@ using namespace std;
 
 int solution(string number) {
     int answer = 0;
-    int num = 0;
-    for(int i = 0; i < number.length(); i++) {
-        num += number[i] - '0';
-    }
-    answer = num % 9;
     return answer;
 }
