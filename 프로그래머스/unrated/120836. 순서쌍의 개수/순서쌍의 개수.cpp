df251fb7525c1e7b1152b9ba9d03f0b902ef0c85
sanghyun0603 <110641935+sanghyun0603@users.noreply.github.com>
2023-11-06 23:55:55 +0900

    [level unrated] Title: 순서쌍의 개수, Time: 0.01 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120836. 순서쌍의 개수/순서쌍의 개수.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    for(int i = 1 ; i <= sqrt(n); i++) {
+        if(n%i == 0) {
+            answer ++;
+            if(i != n/i) answer++;
+        }
+    }
+    return answer;
+}
