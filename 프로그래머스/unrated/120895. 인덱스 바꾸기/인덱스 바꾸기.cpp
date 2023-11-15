e4f85ddfc91069c7f96a6d97fc8966b57929eea2
sanghyun0603 <110641935+sanghyun0603@users.noreply.github.com>
2023-11-15 23:30:48 +0900

    [level unrated] Title: 인덱스 바꾸기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120895. 인덱스 바꾸기/인덱스 바꾸기.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string, int num1, int num2) {
+    string answer = "";
+    char a = my_string[num1];
+    my_string[num1] = my_string[num2];
+    my_string[num2] = a;
+    return my_string;
+}
