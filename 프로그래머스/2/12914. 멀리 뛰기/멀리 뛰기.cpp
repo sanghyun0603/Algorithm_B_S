a2ba4a2e6c40e6e99b0cd3fb9fe3ec1b88ef3abd
sanghyun0603 <110641935+sanghyun0603@users.noreply.github.com>
2024-02-06 23:51:37 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.02 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+long long solution(int n) {
+    long long answer = 0;
+    long long arr[2001] = {};
+    arr[1] = 1;
+    arr[2] = 2;
+    for(int i = 3; i <= n; i++) {
+        arr[i] = (arr[i-1] + arr[i-2])%1234567;
+    }
+    return arr[n];
+}
