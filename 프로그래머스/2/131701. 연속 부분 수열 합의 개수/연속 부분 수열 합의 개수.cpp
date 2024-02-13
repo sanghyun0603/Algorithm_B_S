ff844b56e165ef9bc268615bbfd376e8a3f12d05
sanghyun0603 <110641935+sanghyun0603@users.noreply.github.com>
2024-02-13 16:09:52 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 160.39 ms, Memory: 23.3 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.cpp
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.cpp
@@ -5,19 +5,23 @@ using namespace std;
 
 int solution(vector<int> elements) {
     int answer = 0;
-    int sums = 0;
     int ele_len = elements.size();
     set<int> s;
-    for(int lens = 1; lens <= ele_len; lens++) {
-        for(int i = 0; i < ele_len; i++) {
-            sums = 0;
-            for(int j = i; j < lens + i; j++) {
-                if(j >= ele_len) sums += elements[j - ele_len];
-                else sums += elements[j];
-            }
+
+    for (int lens = 1; lens <= ele_len; lens++) {
+        int sums = 0;
+
+        for (int i = 0; i < lens; i++) {
+            sums += elements[i];
+        }
+        s.insert(sums);
+
+        for (int i = 1; i < ele_len; i++) {
+            sums = sums - elements[i - 1] + elements[(i + lens - 1) % ele_len];
             s.insert(sums);
         }
     }
+
     answer = s.size();
     return answer;
 }
