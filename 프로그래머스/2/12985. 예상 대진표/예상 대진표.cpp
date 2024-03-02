499432158f82be80813b8d1981ecc96c595d9156
sanghyun0603 <110641935+sanghyun0603@users.noreply.github.com>
2024-03-02 23:08:04 +0900

    [level 2] Title: 예상 대진표, Time: 0.03 ms, Memory: 3.66 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int solution(int n, int a, int b)
+{
+    int answer = 0;
+    while(a != b) {
+        a = (a / 2) + (a % 2);
+        b = (b / 2) + (b % 2);
+        answer++;
+    }
+
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    cout << "Hello Cpp" << endl;
+
+    return answer;
+}
