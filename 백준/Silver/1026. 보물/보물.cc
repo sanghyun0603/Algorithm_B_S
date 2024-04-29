3aaf35619aa34c4f0df7219fc27ca444e4e101b8
sanghyun0603 <110641935+sanghyun0603@users.noreply.github.com>
2024-04-29 18:57:00 +0900

    [Silver IV] Title: 보물, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main(void)
+{
+	int num;
+    vector<int> a;
+    vector<int> b;
+    int N;
+	cin >> N;
+	for (int i = 0; i < N; i++)
+	{
+		cin >> num;
+		a.push_back(num);
+	}
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> num;
+		b.push_back(num);
+	}
+
+	sort(a.begin(), a.end());
+	sort(b.begin(), b.end(), greater<>());
+	int res = 0;
+	for (int i = 0; i < N; i++) {
+		res += a[i] * b[i];
+	}
+	cout << res;
+}
