aadfa7dfde686ba45347eb520d267133fe34b000
sanghyun0603 <110641935+sanghyun0603@users.noreply.github.com>
2024-06-27 18:20:41 +0900

    [Bronze II] Title: 커트라인, Time: 104 ms, Memory: 9380 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+let input1 = input[0].split(" ");
+const num = input1[0];
+const award = input1[1];
+
+const grade = input[1].split(" ");
+
+grade.sort((a,b) => b - a);
+
+console.log(grade[award - 1]);
