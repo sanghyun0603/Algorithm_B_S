18ff3bc2c3b54f8005479f063d70e95487de17d3
sanghyun0603 <110641935+sanghyun0603@users.noreply.github.com>
2024-09-03 17:27:03 +0900

    [Silver V] Title: 아무래도이문제는A번난이도인것같다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1402. 아무래도이문제는A번난이도인것같다/아무래도이문제는A번난이도인것같다.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    int t;
+    int a,b;
+    cin >> t;
+    for(int i = 0; i < t; i++) {
+        cin >> a >> b;
+        cout << "yes" << '\n';
+    }
+    return 0;
+}
