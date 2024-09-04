c6edecea449cb4ede2c81ced9b7c41797088d057
sanghyun0603 <110641935+sanghyun0603@users.noreply.github.com>
2024-09-04 15:52:24 +0900

    [Silver IV] Title: 대칭 차집합, Time: 264 ms, Memory: 20768 KB -BaekjoonHub

--- 백준/Silver/1269. 대칭 차집합/대칭 차집합.cc
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.cc
@@ -3,13 +3,12 @@
 #include <vector>
 
 using namespace std;
-// 자연수를 원소로 갖는 공집합이 아닌 두 집합 A와 B가 있다.
-//  이때, 두 집합의 대칭 차집합의 원소의 개수를 출력하는 프로그램을 작성하시오.
-//   두 집합 A와 B가 있을 때, (A-B)와 (B-A)의 합집합을 A와 B의 대칭 차집합이라고 한다.
-// 예를 들어, A = { 1, 2, 4 } 이고, B = { 2, 3, 4, 5, 6 } 라고 할 때,
-//   A-B = { 1 } 이고, B-A = { 3, 5, 6 } 이므로, 대칭 차집합의 원소의 개수는 1 + 3 = 4개이다.
+
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
     int n, m, num;
     int cnt = 0;
     map<int, int> nums;
