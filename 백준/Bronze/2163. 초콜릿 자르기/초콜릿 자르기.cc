6371078a0c64c22ff7561ac2299280685b440925
sanghyun0603 <110641935+sanghyun0603@users.noreply.github.com>
2024-11-12 01:03:01 +0900

    [Bronze I] Title: 초콜릿 자르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2163. 초콜릿 자르기/초콜릿 자르기.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int r,c;
+    cin >> r >> c;
+    cout << r*c -1 << '\n';
+    return 0;
+}
+
+//2 1 = 1
+//1 1 = 0
+//2 2 = 3
+//2 3 = 5
+//2 4 = 7
+//3 3 = 8
